0362e3e38ed84e4e7aaf07f944dfce6fb9998060
sasshu <winter12shu27@gmail.com>
2024-04-23 22:57:44 +0900

    electron-storeからkeygroup名の取得

--- src/index/index.js
+++ src/index/index.js
@@ -1,17 +1,35 @@
-window.addEventListener("DOMContentLoaded", () => {
-  // electron-store
-  // 値を保存
-  setStore = (key, value) => {
-    window.store.set(key, value);
-  };
-  // 保存された値を取得
-  getValue = async (key) => {
-    return window.store.get(key);
-  };
-  // 保存された値を削除
-  deleteValue = (key) => {
-    window.store.delete(key);
-  };
+// electron-store
+// 値を保存
+setStore = (key, value) => {
+  window.store.set(key, value);
+};
+// 保存された値を取得
+getValue = async (key) => {
+  return window.store.get(key);
+};
+// 保存された値を削除
+deleteValue = (key) => {
+  window.store.delete(key);
+};
+
+// キーグループのリストを表示
+buildKeygroups = async () => {
+  const keygroups = await getValue("keygroups");
+  console.log(keygroups);
+  const keygroupsHtmls = [];
+  for (const keygroup of keygroups) {
+    keygroupsHtmls.push(`
+      <li class="border-b-2 pt-2">
+      <button class="key-detail-button">${keygroup.name}</button>
+      </li>`);
+  }
+
+  const keygroupsWrapper = document.querySelector("#key-group-list");
+  keygroupsWrapper.innerHTML = keygroupsHtmls.join("");
+};
+
+window.addEventListener("DOMContentLoaded", async () => {
+  await buildKeygroups();
 
   // キーグループを追加
   const keygroupAddButton = document.querySelector("#keygroup-add-button");
