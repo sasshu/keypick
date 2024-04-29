c1beee3af149e60d2982aa9e01cf89972bb36dfd
sasshu <winter12shu27@gmail.com>
2024-04-29 22:36:54 +0900

    キーグループの順序変更機能作成（データ連動）

--- src/index/index.js
+++ src/index/index.js
@@ -1,12 +1,12 @@
 window.addEventListener("DOMContentLoaded", async () => {
-  const keygroups = await window.store.get("keygroups");
-  console.log(keygroups);
+  let keyGroups = await window.store.get("keygroups");
+  console.log(keyGroups);
   let draggedElement;
   const dropZoneHtml = "<div class='key-drop-zone py-1'></div>";
-  buildKeygroupsHtml(keygroups);
+  buildKeyGroupsHtml(keyGroups);
 
   // キーグループの順序入れ替え
-  const keyElements = document.querySelectorAll('li[draggable="true"]');
+  const keyElements = document.querySelectorAll(".key-group");
   keyElements.forEach((keyElement) => {
     // ドラッグが開始したときに発生
     keyElement.addEventListener("dragstart", (event) => {
@@ -20,6 +20,46 @@ window.addEventListener("DOMContentLoaded", async () => {
     registerDragAndDropEvent(dropZone);
   });
 
+  // キーグループを追加
+  const keyGroupAddButton = document.querySelector("#keygroup-add-button");
+  keyGroupAddButton.addEventListener("click", window.api.addKeyGroup);
+
+  // キー詳細ページへ遷移
+  const keyDetailButtons = document.querySelectorAll(".key-detail-button");
+  keyDetailButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const keyGroupIndex = keyGroups.findIndex(
+        (group) => group.id == button.name
+      );
+      window.api.passKeyGroupIndex(keyGroupIndex);
+      location.href = "../detail/detail.html";
+    });
+  });
+
+  // ダークモードの切り替え
+  const themeChangeButton = document.querySelector("#theme-change-button");
+  themeChangeButton.addEventListener("click", window.api.toggleDarkmode);
+
+  // キーグループのリストを表示
+  function buildKeyGroupsHtml(keyGroups) {
+    const keyGroupsHtmls = [];
+    keyGroups.forEach((keyGroup, index) => {
+      keyGroupsHtmls.push(`
+        ${dropZoneHtml}
+        <li class="border-b-2 pt-2 key-group" draggable="true">
+          <button class="key-detail-button hover:opacity-50"
+            name="${keyGroup.id}">
+            ${keyGroup.name}
+          </button>
+        </li>
+        ${index === keyGroups.length - 1 ? dropZoneHtml : ""}
+      `);
+    });
+
+    const keyGroupsWrapper = document.querySelector("#key-group-list");
+    keyGroupsWrapper.innerHTML = keyGroupsHtmls.join("");
+  }
+
   function registerDragAndDropEvent(dropZone) {
     addEventToDragOver(dropZone);
     addEventToDragLeave(dropZone);
@@ -30,7 +70,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // ドラッグ要素がターゲットの上にあるときに発生
     dropZone.addEventListener("dragover", (event) => {
       event.preventDefault();
-      // ドラッグ要素の両脇への移動は禁止
+      // ドラッグ要素の前後への移動は禁止
       if (
         draggedElement.previousElementSibling !== dropZone &&
         draggedElement.nextElementSibling !== dropZone
@@ -45,7 +85,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // ドラッグ要素がターゲットの上から離れたときに発生
     dropZone.addEventListener("dragleave", (event) => {
       event.preventDefault();
-      // ドラッグ要素の両脇への移動は禁止
+      // ドラッグ要素の前後への移動は禁止
       if (
         draggedElement.previousElementSibling !== event.target &&
         draggedElement.nextElementSibling !== event.target
@@ -60,7 +100,7 @@ window.addEventListener("DOMContentLoaded", async () => {
     // ドラッグ要素がドロップされたときに発生
     dropZone.addEventListener("drop", (event) => {
       event.preventDefault();
-      // ドラッグ要素の両脇への移動は禁止
+      // ドラッグ要素の前後への移動は禁止
       if (
         draggedElement.previousElementSibling !== event.target &&
         draggedElement.nextElementSibling !== event.target
@@ -69,56 +109,31 @@ window.addEventListener("DOMContentLoaded", async () => {
         event.target.classList.add("py-1");
 
         const draggedParent = draggedElement.parentNode;
-        const keygroupsWrapper = document.querySelector("#key-group-list");
+        const keyGroupsWrapper = document.querySelector("#key-group-list");
         // 元の場所から削除
         draggedParent.removeChild(draggedElement.previousElementSibling);
         // 移動先に追加
         const newDropZone = event.target.cloneNode();
-        keygroupsWrapper.insertBefore(draggedElement, event.target);
-        keygroupsWrapper.insertBefore(newDropZone, draggedElement);
+        keyGroupsWrapper.insertBefore(draggedElement, event.target);
+        keyGroupsWrapper.insertBefore(newDropZone, draggedElement);
+
+        // querySelectorAll()で取得できるのはNodeListなので、Arrayに変換する
+        const orderedKeyGroupIdList = Array.from(
+          document.querySelectorAll(".key-detail-button"),
+          (button) => button.name
+        );
+
+        // データ順を変更
+        const newKeyGroups = [];
+        orderedKeyGroupIdList.forEach((id) => {
+          const newKeyGroup = keyGroups.find((keyGroup) => keyGroup.id == id);
+          newKeyGroups.push(newKeyGroup);
+        });
+        keyGroups = structuredClone(newKeyGroups);
+        window.store.set("keygroups", keyGroups);
 
         registerDragAndDropEvent(newDropZone);
       }
     });
-  }
-
-  // キーグループを追加
-  const keygroupAddButton = document.querySelector("#keygroup-add-button");
-  keygroupAddButton.addEventListener("click", window.api.addKeygroup);
-
-  // キー詳細ページへ遷移
-  const keyDetailButtons = document.querySelectorAll(".key-detail-button");
-  keyDetailButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const keygroupIndex = keygroups.findIndex(
-        (group) => group.id == button.name
-      );
-      window.api.passKeyGroupIndex(keygroupIndex);
-      location.href = "../detail/detail.html";
-    });
-  });
-
-  // ダークモードの切り替え
-  const themeChangeButton = document.querySelector("#theme-change-button");
-  themeChangeButton.addEventListener("click", window.api.toggleDarkmode);
-
-  // キーグループのリストを表示
-  function buildKeygroupsHtml(keygroups) {
-    const keygroupsHtmls = [];
-    keygroups.forEach((keygroup, index) => {
-      keygroupsHtmls.push(`
-        ${dropZoneHtml}
-        <li class="border-b-2 pt-2" draggable="true">
-          <button class="key-detail-button hover:opacity-50"
-            name="${keygroup.id}">
-            ${keygroup.name}
-          </button>
-        </li>
-        ${index === keygroups.length - 1 ? dropZoneHtml : ""}
-      `);
-    });
-
-    const keygroupsWrapper = document.querySelector("#key-group-list");
-    keygroupsWrapper.innerHTML = keygroupsHtmls.join("");
   }
 });
